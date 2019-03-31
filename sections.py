d50494f1d2d5258ea42963368d1eb12b340791d1
Ryan Wilson <rwils83@gmail.com>
2019-03-31 10:19:27 -0400

    updated file structure

--- sections.py
+++
@@ -1,8 +0,0 @@
-# sections.py
-from elftools.elf.elffile import ELFFile
-
-
-with open('./chall.elf', 'rb') as f:
-    e = ELFFile(f)
-    for section in e.iter_sections():
-        print(hex(section['sh_addr']), section.name)
